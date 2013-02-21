e1cc0da7f8ad99eff3109783b533743d17b65d15
Brian Radak <radakb@biomaps.rutgers.edu>
2013-02-21 11:08:09 -0500

    oops, this was an old idea that I dropped...

--- CDI/amberio/amberhamiltonian.py
+++
@@ -1,5 +0,0 @@
-from amberrun import AmberRun
-class AmberHamiltonian(AmberRun):
-    def __init__(self,mode=None,basename=None,**filenames):
-        AmberRun.__init__(self,mode=None,basename=None,**filenames)
-        
